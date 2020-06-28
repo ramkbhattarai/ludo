4cb5290bd8d9b28cfbc9a20aee4f97c2b1efdf86
Ram <ramkb466@gmail.com>
2020-06-28 11:10:53 -0400

    displayed dice face with click

--- logic.js
+++ logic.js
@@ -1,44 +1,53 @@
 
-let dice = document.getElementById('number').innerHTML = wordLength;
-let collection = document.getElementById('collection');
-let chances = 6;
-let strike = 0;
-for (let i = 0; i < wordLength; i++) {
-    let ele = document.createElement('li');
-    collection.append(ele);
-    ele.innerHTML = '__';
-}
-let input = document.getElementById('input');
-let check = '';
-input.addEventListener('change', () => {
+let dice = document.getElementById("diceValueMain");
+console.log(dice);
+dice.addEventListener('click',() => {
+    let number =Math.floor( Math.random()* 6 + 1);
+    
+    console.log(number);
+    dice.style.backgroundImage = "url(./images/"+`${number}`+".png)";
 
-    let char = input.value.toLowerCase();
+   
+})
+// let collection = document.getElementById('collection');
+// let chances = 6;
+// let strike = 0;
+// for (let i = 0; i < wordLength; i++) {
+//     let ele = document.createElement('li');
+//     collection.append(ele);
+//     ele.innerHTML = '__';
+// }
+// let input = document.getElementById('input');
+// let check = '';
+// input.addEventListener('change', () => {
 
-    input.value = "";
-    if (word.includes(char)) {
-        check += char;
-        for (let i = 0; i < word.length; i++) {
-            if (word[i] === char) {
-                collection.children[i].innerHTML = char;
-            }
-        }
-    } else {
-        let li = document.createElement('li');
-        document.getElementById('guessed').append(li);
-        li.innerHTML = char;
-        document.getElementById('length').innerHTML = --chances;
-        document.getElementById('image').src = `./images/strike-${++strike}.png`;
-    }
+//     let char = input.value.toLowerCase();
 
-    if (check === word) {
-        let h = document.createElement('h2');
-        document.getElementById('guessed').append(h);
-        h.innerHTML = "Congraulation You Won";
-    }
-    if (!chances) {
-        let h = document.createElement('h2');
-        document.getElementById('guessed').append(h);
-        h.innerHTML = "Game Over";
-    }
+//     input.value = "";
+//     if (word.includes(char)) {
+//         check += char;
+//         for (let i = 0; i < word.length; i++) {
+//             if (word[i] === char) {
+//                 collection.children[i].innerHTML = char;
+//             }
+//         }
+//     } else {
+//         let li = document.createElement('li');
+//         document.getElementById('guessed').append(li);
+//         li.innerHTML = char;
+//         document.getElementById('length').innerHTML = --chances;
+//         document.getElementById('image').src = `./images/strike-${++strike}.png`;
+//     }
 
-});
+//     if (check === word) {
+//         let h = document.createElement('h2');
+//         document.getElementById('guessed').append(h);
+//         h.innerHTML = "Congraulation You Won";
+//     }
+//     if (!chances) {
+//         let h = document.createElement('h2');
+//         document.getElementById('guessed').append(h);
+//         h.innerHTML = "Game Over";
+//     }
+
+// });
