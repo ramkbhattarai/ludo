ba80c9b2ea9f69bbf9b68df4304b2b0de4f73214
Ram <ramkb466@gmail.com>
2020-07-07 22:21:07 -0400

    pulled the elements of rbgy

--- logic.js
+++ logic.js
@@ -8,3 +8,14 @@ dice.addEventListener('click',() => {
 
    
 })
+var step = document.getElementsByClassName('astep');
+var rdice = document.getElementsByClassName('r-dice');
+var gdice = document.getElementsByClassName('g-dice');
+var bdice = document.getElementsByClassName('b-dice');
+var ydice = document.getElementsByClassName('y-dice');
+
+var dicesound = document.getElementById("diceSound");
+var killedsound = document.getElementById("killedSound");
+var winsound = document.getElementById("winSound");
+var inoutsound = document.getElementById("inoutSound");
+var stepsound = document.getElementById("stepSound"); 
